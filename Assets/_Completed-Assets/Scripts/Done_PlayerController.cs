12515e5e40aa8d1565a79543218bc6bf7dc1cd96
g-bunny <astarrynite@gmail.com>
2016-11-23 18:52:20 -0500

    adjusting player movement to incorporate rotation with horizontal keys, and forward/backward movement in relation to transform's direction with vertical keys

--- Assets/_Completed-Assets/Scripts/Done_PlayerController.cs
+++ Assets/_Completed-Assets/Scripts/Done_PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 [System.Serializable]
-public class Done_Boundary 
+public class Done_Boundary
 {
 	public float xMin, xMax, zMin, zMax;
 }
@@ -10,6 +10,7 @@ public class Done_Boundary
 public class Done_PlayerController : MonoBehaviour
 {
 	public float speed;
+	public float rotateSpeed;
 	public float tilt;
 	public Done_Boundary boundary;
 
@@ -18,32 +19,36 @@ public class Done_PlayerController : MonoBehaviour
 	public float fireRate;
 	 
 	private float nextFire;
-	
+
 	void Update ()
 	{
-		if (Input.GetButton("Fire1") && Time.time > nextFire) 
-		{
+		if (Input.GetKey (KeyCode.Space) && Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
-			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-			GetComponent<AudioSource>().Play ();
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+			GetComponent<AudioSource> ().Play ();
 		}
 	}
 
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float angle = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		GetComponent<Rigidbody>().velocity = movement * speed;
-		
-		GetComponent<Rigidbody>().position = new Vector3
-		(
-			Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+		Vector3 movement = transform.forward * moveVertical;
+
+		rotate (angle * rotateSpeed);
+		GetComponent<Rigidbody> ().velocity = movement * speed;
+
+
+		GetComponent<Rigidbody> ().position = new Vector3 (
+			Mathf.Clamp (GetComponent<Rigidbody> ().position.x, boundary.xMin, boundary.xMax), 
 			0.0f, 
-			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+			Mathf.Clamp (GetComponent<Rigidbody> ().position.z, boundary.zMin, boundary.zMax)
 		);
-		
-		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+	}
+
+	void rotate (float angles)
+	{
+		transform.RotateAround (transform.position, transform.up, angles);
 	}
 }
